bc11a32da7500820c95ab2f03c0b3cd63daf7022
rleidy3@gatech.edu <rleidy3@gatech.edu@d9b7ffe3-4506-c245-db09-9c0bbb57965b>
2011-10-27 20:24:33 +0000

    added some regex functions

--- trunk/part-1/src/Parser.java
+++ trunk/part-1/src/Parser.java
@@ -4,10 +4,45 @@ public class Parser {
 	
 	Char currentToken;
 	
+	//returns the current token without consuming it
 	public String peekToken(){
 		return currentToken;
 	}
-	matchToken(){
+	
+	//takes in a token and matches it the regex language to make sure it is valid
+	matchToken(Token tok){
+		
+	}
+	
+	/*<reg-ex>  <rexp> 
+	<rexp> <rexp1> <rexp$>
+	<rexp$>  UNION <rexp1> <rexp$>  |  E         
+	<rexp1>  (<rexp>) <rexp1-tail>  | RE_CHAR <rexp1-tail1> | <rexp2>
+	<rexp1-tail>  * <rexp> | + <rexp> |  E 
+	<rexp1-tail1>  <rexp1-tail2> | <rexp>     
+	<rexp1-tail2>  * <rexp> | + <rexp>
+	<rexp2>  <char-class> <rexp>  |  E  
+	<char-class>  .  |  [ <char-class1>  | <defined-class>
+	<char-class1>   <char-set> | <exclude-set>
+	<char-set>  CLS_CHAR ]  |  <range>
+	<range>  CLS_CHAR – CLS_CHAR ]
+	<exclude-set>  ^ <char-set>] IN <exclude-set-tail>  
+	<exclude-set-tail>  [<char-set>  | <defined-class>*/
+	reg-ex(){
+		rexp();
+	}
+	
+	rexp(){
+		rexp1();
+		rexp$();
+	}
+	rexp$(){
+		
+	}
+	public static void main(String[] args){
+
+		File inputFile = "example.txt";
+		SuperScanner supScan = new SuperScanner(inputFile);
 		
 	}
 }
