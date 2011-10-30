93d85c6119be6236a2dee3d26c30306e9a10f905
rkernan@gmail.com <rkernan@gmail.com@d9b7ffe3-4506-c245-db09-9c0bbb57965b>
2011-10-30 21:29:46 +0000

    emptied input buffer

--- trunk/part-1/src/InputBuffer.java
+++ trunk/part-1/src/InputBuffer.java
@@ -11,8 +11,7 @@ import java.util.Scanner;
 public class InputBuffer {
 
 	private Scanner input;
-	private String buffer;
-	private int currentpos;
+	private char current;
 	
 	/**
 	 * setup the buffer with given scanner
@@ -20,29 +19,5 @@ public class InputBuffer {
 	 */
 	public InputBuffer(Scanner input) {
 		this.input = input;
-	}
-	
-	/**
-	 * get the next character in the buffer
-	 * @return next character in the buffer
-	 */
-	public char getNext() {
-		if(currentpos > buffer.length() -1) {
-			this.buffer = input.nextLine();
-			this.currentpos = 0;
-			return '\n';
-		}
-		else {
-			return this.buffer.charAt(currentpos++);
-		}
-	}
-	
-	/**
-	 * move the buffer back a character
-	 */
-	public void backup() {
-		if(this.currentpos > 0) {
-			this.currentpos--;
-		}
 	}
 }
