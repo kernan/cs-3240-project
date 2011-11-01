f1ba8b918a4e25cc86bea4f9a0417086b85194d1
rkernan@gmail.com <rkernan@gmail.com@d9b7ffe3-4506-c245-db09-9c0bbb57965b>
2011-11-01 23:46:52 +0000

    InputBuffer is fixed

--- trunk/part-1/src/InputBuffer.java
+++ trunk/part-1/src/InputBuffer.java
@@ -1,23 +1,49 @@
 /**
- * A buffer that allows forward a backward stepping through
- * a Scanner input stream.
- * 
  * @author Robert Kernan
- *
+ * 
+ * cs3240 HW-1
  */
 
 import java.util.Scanner;
 
-public class InputBuffer {
+/** InputBuffer.java
+ *	Allows forward and backward stepping through an input stream.
+ */
 
-	private Scanner input;
-	private char current;
+public class InputBuffer {
 	
-	/**
-	 * setup the buffer with given scanner
-	 * @param input scanner to use
+	private Scanner input;
+	private String buffer;
+	int currentpos;
+	
+	/** setup the buffer with a given string
+	 *	@param buffer
 	 */
 	public InputBuffer(Scanner input) {
 		this.input = input;
+		this.buffer = input.nextLine();
+		this.currentpos = 0;
+	}
+	
+	/** get the next character in the buffer
+	 *	@return the next character in the buffer
+	 */
+	public char getNext() {
+		if(currentpos > buffer.length() - 1) {
+			this.buffer = input.nextLine();
+			this.currentpos = 0;
+			return '\n';
+		}
+		else {
+			return this.buffer.charAt(currentpos++);
+		}
+	}
+	
+	/** move the buffer position back 1 character 
+	 */
+	public void backup() {
+		if(this.currentpos > 0) {
+			this.currentpos--;
+		}
 	}
 }
