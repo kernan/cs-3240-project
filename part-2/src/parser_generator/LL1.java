526ced73108dbc82ec648b85e9a9fb164403855c
pyroalienmaniac@gmail.com <pyroalienmaniac@gmail.com@d9b7ffe3-4506-c245-db09-9c0bbb57965b>
2011-12-04 22:03:19 +0000

    added pseudocode to LL1 for first and follow sets

--- part-2/src/parser_generator/LL1.java
+++ part-2/src/parser_generator/LL1.java
@@ -7,3 +7,47 @@ package parser_generator;
 public class LL1 {
 	//TODO
 }
+
+/* Algorithm for computing First(A) for all nonterminals A
+ * Pseudocode from book
+ * 
+
+FOR all nonterminals A DO First(A) := {};
+WHILE there are changes to any First(A) do
+	FOR each production choice A --> X1X2...Xn DO
+		k:= 1 ; Continue := true ;
+		WHILE Continue = true AND k <= n DO
+			add First(Xk)-{epsilon} to First(A);
+			IF epsilon is not in First(Xk) THEN Continue := false ;
+			k := k + 1 ;
+		IF Continue = true THEN add epsilon to First(A) ;
+
+**********
+* Simplified algorithm of above in the absense of epsilon-productions
+* We will have to use the first one, but this one makes it easier to understand
+* the basic algorithm
+* 
+
+FOR all nonterminals A DO First(A) := {};
+WHILE there are changes to any First(A) DO
+	FOR each production choice A --> X1X2...Xn DO
+		add First(X1) to First(A);
+
+
+*/
+
+
+/* Algorithm for the computation of Follow sets
+ * Pseudocode from book
+
+Follow(start-symbol) := {$} ;
+FOR all nonterminals A != start-symbol DO Follow(A) := {} ;
+WHILE there are changes to any Follow sets DO
+	FOR each production A --> X1X2...Xn DO
+		FOR EACH Xi that is a nonterminal DO
+			add First(Xi+1Xi+2...Xn) - {epsilon} to Follow(Xi)
+			(* Note: if i=n, then Xi+1Xi+2...Xn = epsilon *)
+			IF epsilon is in First(Xi+1Xi+2...Xn) THEN
+				add Follow(A) to Follow(Xi
+
+*/
