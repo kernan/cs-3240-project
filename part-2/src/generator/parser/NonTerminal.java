190296534986db13a37a0b7c210e25f81266099f
pyroalienmaniac@gmail.com <pyroalienmaniac@gmail.com@d9b7ffe3-4506-c245-db09-9c0bbb57965b>
2011-12-08 22:29:41 +0000

    Fixed LL1_Token Structure

--- part-2/src/generator/parser/NonTerminal.java
+++ part-2/src/generator/parser/NonTerminal.java
@@ -8,25 +8,15 @@ import java.util.ArrayList;
  *
  */
 public class NonTerminal extends LL1_Token {
-	private Token<LL1_TokenType> token;
-	private ArrayList<Token<LL1_TokenType>> FirstSet;
+	
 	private ArrayList<Token<LL1_TokenType>> FollowSet;
 	
-	/**
-	 * 
-	 * @param token
-	 */
-	public NonTerminal(Token<LL1_TokenType> token){
-		this.token = token;
-		this.FirstSet = new ArrayList<Token<LL1_TokenType>>();
+	public NonTerminal(Token<LL1_TokenType> token) {
+		super(token);
+		FollowSet = new ArrayList<Token<LL1_TokenType>>();
 	}
-	/**
-	 * 
-	 * @param tok
-	 */
-	public void addToFirstSet(Token<LL1_TokenType> tok){
-		FirstSet.add(tok);	
-	}
+	
+	
 	
 	/**
 	 * 
@@ -36,21 +26,13 @@ public class NonTerminal extends LL1_Token {
 		FollowSet.add(tok);	
 	}
 	
+	
+	
 	/* Accessors and Mutators	
 	 */	
-	
-	public Token<LL1_TokenType> getToken() {
-		return token;
-	}
-	public void setToken(Token<LL1_TokenType> token) {
-		this.token = token;
-	}
-	public ArrayList<Token<LL1_TokenType>> getFirstSet() {
-		return FirstSet;
-	}
-	public void setFirstSet(ArrayList<Token<LL1_TokenType>> firstSet) {
-		FirstSet = firstSet;
-	}
+
+
+
 	public ArrayList<Token<LL1_TokenType>> getFollowSet() {
 		return FollowSet;
 	}
