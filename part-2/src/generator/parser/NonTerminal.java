dedfd938e8128de5b80dc9f58f0f8ee9b0ee64e4
pyroalienmaniac@gmail.com <pyroalienmaniac@gmail.com@d9b7ffe3-4506-c245-db09-9c0bbb57965b>
2011-12-08 21:29:34 +0000

    Added Terminal and NonTerminal

---
+++ part-2/src/generator/parser/NonTerminal.java
@@ -0,0 +1,66 @@
+package generator.parser;
+
+import global.Token;
+
+import java.util.ArrayList;
+/**
+ * 
+ *
+ */
+public class NonTerminal {
+	private Token<LL1_TokenType> token;
+	private ArrayList<Token<LL1_TokenType>> FirstSet;
+	private ArrayList<Token<LL1_TokenType>> FollowSet;
+	
+	/**
+	 * 
+	 * @param token
+	 */
+	public NonTerminal(Token<LL1_TokenType> token){
+		this.token = token;
+		this.FirstSet = new ArrayList<Token<LL1_TokenType>>();
+	}
+	/**
+	 * 
+	 * @param tok
+	 */
+	public void addToFirstSet(Token<LL1_TokenType> tok){
+		FirstSet.add(tok);	
+	}
+	
+	/**
+	 * 
+	 * @param tok
+	 */
+	public void addToFollowSet(Token<LL1_TokenType> tok){
+		FollowSet.add(tok);	
+	}
+	
+	/* Accessors and Mutators	
+	 */	
+	
+	public Token<LL1_TokenType> getToken() {
+		return token;
+	}
+	public void setToken(Token<LL1_TokenType> token) {
+		this.token = token;
+	}
+	public ArrayList<Token<LL1_TokenType>> getFirstSet() {
+		return FirstSet;
+	}
+	public void setFirstSet(ArrayList<Token<LL1_TokenType>> firstSet) {
+		FirstSet = firstSet;
+	}
+	public ArrayList<Token<LL1_TokenType>> getFollowSet() {
+		return FollowSet;
+	}
+	public void setFollowSet(ArrayList<Token<LL1_TokenType>> followSet) {
+		FollowSet = followSet;
+	}
+	
+	
+	
+	
+	
+	
+}
