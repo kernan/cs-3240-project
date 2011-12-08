527b1693a33006a062a0c4c3c6a463b988df6b48
pyroalienmaniac@gmail.com <pyroalienmaniac@gmail.com@d9b7ffe3-4506-c245-db09-9c0bbb57965b>
2011-12-08 22:29:41 +0000

    Fixed LL1_Token Structure

--- trunk/part-2/src/generator/parser/LL1_Token.java
+++ trunk/part-2/src/generator/parser/LL1_Token.java
@@ -1,5 +1,49 @@
 package generator.parser;
 
-public class LL1_Token {
+import global.Token;
 
+import java.util.ArrayList;
+
+public class LL1_Token {
+	private Token<LL1_TokenType> token;
+	private ArrayList<Token<LL1_TokenType>> FirstSet;
+	
+	/**
+	 * 
+	 * @param token
+	 */
+	public LL1_Token(Token<LL1_TokenType> token){
+		this.token = token;
+		this.FirstSet = new ArrayList<Token<LL1_TokenType>>();
+	}
+	
+	/**
+	 * 
+	 * @param tok
+	 */
+	public void addToFirstSet(Token<LL1_TokenType> tok){
+		FirstSet.add(tok);	
+	}
+
+	
+	
+/* Accessors and Mutators	
+ */
+	
+	public Token<LL1_TokenType> getToken() {
+		return token;
+	}
+
+	public void setToken(Token<LL1_TokenType> token) {
+		this.token = token;
+	}
+
+	public ArrayList<Token<LL1_TokenType>> getFirstSet() {
+		return FirstSet;
+	}
+
+	public void setFirstSet(ArrayList<Token<LL1_TokenType>> firstSet) {
+		FirstSet = firstSet;
+	}
+	
 }
--- trunk/part-2/src/generator/parser/Terminal.java
+++ trunk/part-2/src/generator/parser/Terminal.java
@@ -7,45 +7,9 @@ import global.Token;
  *
  */
 public class Terminal extends LL1_Token {
-	private Token<LL1_TokenType> token;
-	private ArrayList<Token<LL1_TokenType>> FirstSet;
-	
-	/**
-	 * 
-	 * @param token
-	 */
-	public Terminal(Token<LL1_TokenType> token){
-		this.token = token;
-		this.FirstSet = new ArrayList<Token<LL1_TokenType>>();
-	}
-	
-	/**
-	 * 
-	 * @param tok
-	 */
-	public void addToFirstSet(Token<LL1_TokenType> tok){
-		FirstSet.add(tok);	
-	}
 
-	
-	
-/* Accessors and Mutators	
- */
-	
-	public Token<LL1_TokenType> getToken() {
-		return token;
-	}
-
-	public void setToken(Token<LL1_TokenType> token) {
-		this.token = token;
-	}
-
-	public ArrayList<Token<LL1_TokenType>> getFirstSet() {
-		return FirstSet;
-	}
-
-	public void setFirstSet(ArrayList<Token<LL1_TokenType>> firstSet) {
-		FirstSet = firstSet;
+	public Terminal(Token<LL1_TokenType> token) {
+		super(token);
 	}
 	
 	
